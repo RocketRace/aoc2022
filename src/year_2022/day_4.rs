a6fb2f0c80f383274657dfdf32c83c3e47eb6e14
Olivia Palmu <oliviaspalmu@gmail.com>
2022-12-04 15:15:29 +0200

    Day 4

---
+++ src/year_2022/day_4.rs
@@ -0,0 +1,51 @@
+struct Range {
+    start: u32,
+    end: u32
+}
+
+impl Range {
+    fn from_str(s: &str) -> Self {
+        let (start, end) = s.split_once('-').unwrap();
+        Self { start: start.parse().unwrap(), end: end.parse().unwrap() }
+    }
+
+    fn contains(&self, other: &Range) -> bool {
+        self.start <= other.start && self.end >= other.end
+    }
+
+    fn overlaps_left(&self, other: &Range) -> bool {
+        self.start <= other.start && self.end >= other.start
+    }
+}
+
+#[aoc_generator(day4)]
+fn generator(input: &str) -> Vec<(Range, Range)> {
+    input.lines().map(
+        |line| {
+            let (l, r) = line.split_once(',').unwrap();
+            let left = Range::from_str(l);
+            let right = Range::from_str(r);
+            (left, right)
+        }
+    )
+    .collect()
+}
+
+#[aoc(day4, part1)]
+fn containment(input: &[(Range, Range)]) -> usize {
+    input
+        .iter()
+        .filter(
+            |(l, r)| l.contains(r) || r.contains(l)
+        )
+        .count()
+}
+
+#[aoc(day4, part2)]
+fn overlap(input: &[(Range, Range)]) -> usize {
+    input
+        .iter()
+        .filter(|(l, r)| l.overlaps_left(r) || r.overlaps_left(l))
+        .count()
+}
+
