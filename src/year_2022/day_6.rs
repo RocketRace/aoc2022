2e87721f059359e32a4451af5b64c36b63db35a0
Olivia Palmu <oliviaspalmu@gmail.com>
2022-12-07 01:43:06 +0200

    u32

--- src/year_2022/day_6.rs
+++ src/year_2022/day_6.rs
@@ -1,13 +1,13 @@
 #[aoc_generator(day6)]
-fn generator(input: &[u8]) -> Vec<u64> {
-    input.iter().map(|c| 1 << ((*c - b'A') as u64)).collect()
+fn generator(input: &[u8]) -> Vec<u32> {
+    input.iter().map(|c| 1 << ((*c - b'a') as u32)).collect()
 }
 
-fn bithash(slice: &[u64]) -> u64 {
+fn bithash(slice: &[u32]) -> u32 {
     slice.iter().fold(0, |acc, n| acc ^ n)
 }
 
-fn full_hash(input: &[u64], count: usize) -> usize {
+fn full_hash(input: &[u32], count: usize) -> usize {
     count
         + input
             .windows(count)
@@ -16,7 +16,7 @@ fn full_hash(input: &[u64], count: usize) -> usize {
             .count()
 }
 
-fn rolling_hash(input: &[u64], count: usize) -> usize {
+fn rolling_hash(input: &[u32], count: usize) -> usize {
     // Rolling hash using xor
     // Ensures that each bit is set an even number of times
     // during the roll
@@ -35,18 +35,18 @@ fn rolling_hash(input: &[u64], count: usize) -> usize {
 }
 
 #[aoc(day6, part1)]
-fn with_regular_hash_4(input: &[u64]) -> usize {
+fn with_regular_hash_4(input: &[u32]) -> usize {
     full_hash(input, 4)
 }
 #[aoc(day6, part2)]
-fn with_regular_hash_14(input: &[u64]) -> usize {
+fn with_regular_hash_14(input: &[u32]) -> usize {
     full_hash(input, 14)
 }
 #[aoc(day6, part1, rolling)]
-fn with_rolling_hash_4(input: &[u64]) -> usize {
+fn with_rolling_hash_4(input: &[u32]) -> usize {
     rolling_hash(input, 4)
 }
 #[aoc(day6, part2, rolling)]
-fn with_rolling_hash_14(input: &[u64]) -> usize {
+fn with_rolling_hash_14(input: &[u32]) -> usize {
     rolling_hash(input, 14)
 }
